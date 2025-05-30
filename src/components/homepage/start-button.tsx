9a76cdfe2cfe7907ae7de32339e5fa2981ce104a
wutsqo <urwatilwutsqo16@gmail.com>
2025-05-30 13:34:07 +0700

    feat: add reset functionality to StartButton component

--- src/components/homepage/start-button.tsx
+++ src/components/homepage/start-button.tsx
@@ -3,12 +3,14 @@
 import { useFormStatus } from "react-dom";
 import { LoaderCircle, Play } from "lucide-react";
 import { Button } from "../ui/button";
+import { useGameStore } from "@/providers/game-store-provider";
 
 export function StartButton() {
   const status = useFormStatus();
+  const resetUserState = useGameStore((state) => state.reset);
 
   return (
-    <Button className="w-full max-w-lg mx-auto text-xl" type="submit" size={"lg"}>
+    <Button className="w-full max-w-lg mx-auto text-xl" type="submit" size={"lg"} onClick={() => resetUserState()}>
       {status.pending ? (
         <>
           <LoaderCircle className="animate-spin" />
