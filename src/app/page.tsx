712242cdb8f9e7bbc6de71b7f99484d8ced0f2ea
wutsqo <urwatilwutsqo16@gmail.com>
2025-05-30 13:33:46 +0700

    feat: update Home component layout and adjust welcome message styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ import { getTriviaCategories } from "@/services";
 export default async function Home() {
   const categories = await getTriviaCategories();
   return (
-    <div className="container mx-auto p-4 flex gap-8 min-h-screen max-w-screen-xl">
-      <div className="w-full flex flex-col justify-center text-center lg:text-left gap-2">
+    <div className="container mx-auto p-4 flex gap-8 min-h-screen max-w-screen-xl flex-col lg:flex-row">
+      <div className="w-full flex flex-col justify-center text-center lg:text-left gap-2 py-8">
         <h2 className="text-3xl">Welcome to</h2>
         <h1 className="text-5xl lg:text-6xl">The Trivia Game App</h1>
-        <p className="text-xl mt-6">Answer 10 questions and let&apos;s see how many you get it right!</p>
+        <p className="text-lg mt-4">Answer 10 questions and let&apos;s see how many you get it right!</p>
       </div>
       <div className="flex items-center justify-center shrink-0 w-full max-w-md">
         <Card className="w-full">
